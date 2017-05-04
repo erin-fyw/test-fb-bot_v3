c6ef742fd7ecf2dcdcb64325ad09ad517b649a6c
erin-fyw <erin.fyw@gmail.com>
2017-05-04 15:18:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -762,8 +762,7 @@ function sendQuickReply(recipientId) {
 }
 
 function sendGreetingQuickReply(recipientId){
-  var emoji1 = "\u1f44b";
-  var msg1 = 'Hi，我係UNews'+emoji1;
+  var msg1 = 'Hi，我係UNews'+String.fromCharCode(0x1f44b);
   var msg2 = '我可以幫你留意指定主題嘅資訊同新聞，有新消息嗰陣就會通知你。';
   sendTextMessageWithoutQuickReply(recipientId, msg1);
   setTimeout(function() {
